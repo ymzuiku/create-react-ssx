568c6aff9212b423377920b43f54d64ec5f1cd32
pillar <ymblender@gmail.com>
2021-08-27 04:49:12 +0800

    fix: update_readme

--- README.md
+++ README.md
@@ -2,7 +2,19 @@
 
 ## feature
 
-- 支持 SSR/SSG
+- 支持 SSG / SSR
 - 类似 Next 的约定路由: src/pages 下所有 *.tsx 文件均为页面组件, 文件夹或文件名为 `_` 开头的除外
 - 自动懒加载 (开发环境不生效)
 - 支持 tailwind-jit
+
+## scripts
+
+- num run dev : 启动开发模式
+- num run build : 编译 SSG
+- num run server : 启动 SSR 服务
+
+
+## FQA
+
+- Q: 为什么 npm run dev 样式会延迟加载？
+  - A: tailwind-jit 还未动态编译完
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="lg:text-center">
             <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-              Example
+              Template
             </h2>
             <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               Base React SSG/SSR in Vite
