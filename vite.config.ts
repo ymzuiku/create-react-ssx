335da50f7673ab1030b95f1b4138e6013b9c3911
pillar <ymblender@gmail.com>
2021-09-06 01:27:40 +0800

    feat: add vite proxy example

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ const isProd = process.env.NODE_ENV === "production";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [isProd && legacy({ targets: ["defaults", "not IE 11"] })],
+  // server: {
+  //   proxy: {
+  //     "/test-proxy": {
+  //       target: "http://127.0.0.1:4080",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
