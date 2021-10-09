212915b7d32e2f5b5c11109a1fa5526b82d89193
pillar <ymblender@gmail.com>
2021-10-09 16:26:56 +0800

    feat: ignore jest dir

--- jest.config.js
+++ jest.config.js
@@ -131,9 +131,18 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/cmd/",
+    "/internal/",
+    "/dist/",
+    "/dist_serve/",
+    "/coverage/",
+    "/.vscode/",
+    "/.husky/",
+    "/.public/",
+    "/.scripts/",
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
