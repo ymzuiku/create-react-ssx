0cf2af1f6b8e0585322938e52e63ca29379f6e50
pillar <ymblender@gmail.com>
2021-10-16 04:24:13 +0800

    fix: add all screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   mode: "jit",
-  purge: ["./index.html", "./pages/**/*.tsx"],
+  purge: ["./index.html", "./pages/**/*.tsx", "./pkg/**/*.tsx"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      all: "1px",
+    },
     extend: {},
   },
   variants: {
