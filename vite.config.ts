67eb135491c57e8296261c79cecb69f640f84b75
pillar <ymblender@gmail.com>
2021-11-12 21:26:53 +0800

    feat: add base polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,28 @@ const isProd = process.env.NODE_ENV === "production";
 // 此配置仅应用于前端编译
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [isProd && legacy({ targets: ["defaults", "not IE 11"] })],
+  plugins: [
+    isProd &&
+      legacy({
+        targets: ["defaults", "not IE 11"],
+        polyfills: [
+          "es/array",
+          "es/array-buffer",
+          "es/object",
+          "es/string",
+          "es/number",
+          "es/function",
+          "es/map",
+          "es/math",
+          "es/set",
+          "es/promise",
+          "es/regexp",
+          "es/weak-set",
+          "es/weak-map",
+          "es/date",
+        ],
+      }),
+  ],
   server: {
     proxy: {
       "/mark-proxy": {
